fe5269124e30069bdef3a0648950cd639f703a89
A Stone <astone@heckgrammar.co.uk>
2022-12-07 09:55:23 +0000

    Add project files.

---
+++ UsingBigInteger/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Numerics
+Module Module1
+
+    Sub Main()
+        Dim b As BigInteger
+        b = BigInteger.Pow(2, 1000)
+        Console.WriteLine(b.ToString)
+        b = BigInteger.Add(b, b)
+        Console.WriteLine(b.ToString)
+        b = BigInteger.Multiply(b, 12)
+        Console.WriteLine(b.ToString)
+
+    End Sub
+
+End Module
